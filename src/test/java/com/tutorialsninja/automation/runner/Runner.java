1e4c5895fb0bea0a3c61a606255fdad5d93cfcee
Kerstin Rosin <Kerstin.Rosin@msg.group>
2024-10-17 09:03:50 +0200

    Completed automating all the scenarios in the Login feature file

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"classpath:FeatureFiles/Login.feature",
-							 "classpath:FeatureFiles/Register.feature"},
+@CucumberOptions(features = {"classpath:FeatureFiles/Search.feature",
+							 "classpath:FeatureFiles/Register.feature",
+							 "classpath:FeatureFiles/Login.feature"},
 				 glue = {"classpath:com.tutorialsninja.automation.stepdef"},
 				 plugin = {"pretty",
 						   "html:target/cucumber_html_report.html"})
